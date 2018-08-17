c3ad2258b0d6608744eaa98ab3f576bd2a2d1767
Joe Zou <yixian.zou@gmail.com>
2018-08-17 16:53:01 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# picvuedo
+# picvuido
+
+> A Vuido project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# build
+npm run build
+
+# run the bundled script
+npm start
+```
+
+For detailed explanation on how things work, consult the [Vuido documentation](https://vuido.mimec.org/).
---
+++ src/MainWindow.vue
@@ -0,0 +1,28 @@
+<template>
+  <Window title="Test" width="400" height="100" margined v-on:close="exit">
+    <Box>
+      <Text>Welcome to your Test application!</Text>
+      <Button v-on:click="ok">OK</Button>
+      <Button v-on:click="cancel">Cancel</Button>
+    </Box>
+  </Window>
+</template>
+
+<script>
+import libui from 'libui-node'
+
+export default {
+  methods: {
+    exit() {
+      libui.stopLoop();
+    },
+    ok(){
+      console.log("ok!")
+    },
+    cancel(){
+      console.log("cancel!")
+    }
+
+  }
+}
+</script>

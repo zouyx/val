6dc8c866482020ae396d5bb16202e3b098c9ccff
Joe Zou <yixian.zou@gmail.com>
2018-08-22 23:15:20 +0800

    update cal

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# picvuido
+# val
 
 > A Vuido project
 
--- src/MainWindow.vue
+++ src/MainWindow.vue
@@ -1,28 +1,56 @@
 <template>
-  <Window title="Test" width="400" height="100" margined v-on:close="exit">
-    <Box>
-      <Text>Welcome to your Test application!</Text>
-      <Button v-on:click="ok">OK</Button>
-      <Button v-on:click="cancel">Cancel</Button>
+  <Window title="Test Demo" width="100px" v-on:close="exit">
+    <Box horizontal padded>
+      <Group stretchy title="计算器">
+        <Box>
+          <TextInput v-model="text" readonly="true"/>
+          <Box padded margined horizontal="true">
+            <Button v-on:click="click(0)">0</Button>
+            <Button v-on:click="click(1)">1</Button>
+            <Button v-on:click="click(2)">2</Button>
+            <Button v-on:click="click(3)">3</Button>
+            <Button v-on:click="click(4)">4</Button>
+          </Box>
+          <Box padded margined horizontal="true">
+            <Button v-on:click="click(5)">5</Button>
+            <Button v-on:click="click(6)">6</Button>
+            <Button v-on:click="click(7)">7</Button>
+            <Button v-on:click="click(8)">8</Button>
+            <Button v-on:click="click(9)">9</Button>
+          </Box>
+          <Box padded margined horizontal="true">
+            <Button v-on:click="click('+')">+</Button>
+            <Button v-on:click="click('-')">-</Button>
+            <Button v-on:click="click('*')">*</Button>
+            <Button v-on:click="click('/')">/</Button>
+            <Button v-on:click="cal('=')">=</Button>
+          </Box>
+        </Box>
+      </Group>
     </Box>
   </Window>
 </template>
 
 <script>
-import libui from 'libui-node'
+  import libui from 'libui-node'
 
-export default {
-  methods: {
-    exit() {
-      libui.stopLoop();
+  export default {
+    data() {
+      return {
+        text: '',
+
+      };
     },
-    ok(){
-      console.log("ok!")
-    },
-    cancel(){
-      console.log("cancel!")
+    methods: {
+      exit() {
+        libui.stopLoop();
+      },
+      click(num) {
+        this.text += num;
+      },
+      cal(symbol) {
+        this.text = eval(this.text)
+      },
     }
-
   }
-}
 </script>

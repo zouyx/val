07050de14d1f703e3a09fe1cacf99786061dcde6
Joe Zou <yixian.zou@gmail.com>
2018-08-26 23:13:34 +0800

    修改计算器样式

--- src/MainWindow.vue
+++ src/MainWindow.vue
@@ -1,31 +1,35 @@
 <template>
   <Window title="计算器" width="100" height="100" margined v-on:close="exit" ref="mainWindow">
     <Box horizontal padded>
-      <Group stretchy>
-        <Box>
-          <TextInput v-model="text" readonly="true"/>
-          <Box padded margined horizontal="true">
-            <Button v-on:click="click(0)">0</Button>
-            <Button v-on:click="click(1)">1</Button>
-            <Button v-on:click="click(2)">2</Button>
-            <Button v-on:click="click(3)">3</Button>
-            <Button v-on:click="click(4)">4</Button>
+      <Group stretchy margined>
+        <Box padded>
+          <Box>
+            <TextInput v-model="text" readonly="true"/>
           </Box>
           <Box padded margined horizontal="true">
-            <Button v-on:click="click(5)">5</Button>
-            <Button v-on:click="click(6)">6</Button>
-            <Button v-on:click="click(7)">7</Button>
-            <Button v-on:click="click(8)">8</Button>
-            <Button v-on:click="click(9)">9</Button>
+            <Button stretchy v-on:click="click(1)">1</Button>
+            <Button stretchy v-on:click="click(2)">2</Button>
+            <Button stretchy v-on:click="click(3)">3</Button>
+            <Button stretchy v-on:click="click('+')">+</Button>
           </Box>
           <Box padded margined horizontal="true">
-            <Button v-on:click="click('+')">+</Button>
-            <Button v-on:click="click('-')">-</Button>
-            <Button v-on:click="click('*')">*</Button>
-            <Button v-on:click="click('/')">/</Button>
-            <Button v-on:click="cal('=')">=</Button>
+            <Button stretchy v-on:click="click(4)">4</Button>
+            <Button stretchy v-on:click="click(5)">5</Button>
+            <Button stretchy v-on:click="click(6)">6</Button>
+            <Button stretchy v-on:click="click('-')">-</Button>
           </Box>
-          <Button @click="selectFile">Browse...</Button>
+          <Box padded margined horizontal="true">
+            <Button stretchy v-on:click="click(7)">7</Button>
+            <Button stretchy v-on:click="click(8)">8</Button>
+            <Button stretchy v-on:click="click(9)">9</Button>
+            <Button stretchy v-on:click="click('*')">*</Button>
+          </Box>
+          <Box padded margined horizontal="true">
+            <Button stretchy v-on:click="click(0)">0</Button>
+            <Button stretchy v-on:click="cal('=')">=</Button>
+            <Button stretchy v-on:click="clear()">C</Button>
+            <Button stretchy v-on:click="click('/')">/</Button>
+          </Box>
         </Box>
       </Group>
     </Box>
@@ -52,13 +56,9 @@
       cal(symbol) {
         this.text = eval(this.text)
       },
-      selectFile() {
-        const window = this.$refs.mainWindow.window;
-        const filename = libui.UiDialogs.openFile(window);
-        if (filename) {
-          console.log(filename)
-        }
-      }
+      clear() {
+        this.text = ''
+      },
     }
   }
 </script>
